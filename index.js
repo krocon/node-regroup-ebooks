bf49c2561cef9350f6efe33cab7696fc90d8b244
Marc Kronberg <krocon@gmail.com>
2020-02-08 18:12:48 +0100

    ..

--- index.js
+++ index.js
@@ -5,17 +5,10 @@ import fs from "fs";
 import shell from "shelljs";
 import log from "npmlog";
 import walk from "walkdir";
+import {regroup} from "./regroup.mjs";
 
 
-export function writeFile(file, rows) {
-  let dir = path.dirname(file);
-  shell.mkdir('-p', dir);
-
-  fs.writeFileSync(file, rows.join('\n'), (err) => {
-    if (err) throw err;
-    console.log('Saved!', file);
-  });
-
+function createStructure(rows) {
   const o = {};
   for (let i = 0; i < rows.length; i++) {
     const row = rows[i];
@@ -27,68 +20,79 @@ export function writeFile(file, rows) {
       o[d] = [name];
     }
   }
+  return o;
+}
 
-  fs.writeFileSync('./out/raw.json', JSON.stringify(o, null, 2), (err) => {
+export async function scan(dirarr) {
+  for (const dir of dirarr) {
+    if (!dir) {
+      throw new Error('Parameter dir is missing: ' + dir);
+    }
+  }
+  let back = [];
+  for (const dir of dirarr) {
+    let p = path.normalize(dir);
+    log.info('nsd', 'Scanning ', p);
+
+    const walkOptions = {
+      return_object: true,
+      no_return: false,
+      find_links: false,
+      filter: (p, files) => {
+        // console.info('path', p); // TODO weg
+        // console.info('       files', files); // TODO weg
+
+        if (p.indexOf('.') === 0) return [];
+
+        return files;
+        // return files.filter(n => {
+        //   let ext = path.extname(n);
+        //   return ext === '.js'; // || n.indexOf('.') === -1;
+        // });
+      }
+    };
+    let result = await walk.async(dir, walkOptions);
+
+    const ret = [];
+    for (let [p, stat] of Object.entries(result)) {
+      // if (path.endsWith('.js')) {
+      if (stat.size > 0) {
+        // log.info(`${path} mode:${stat.mode} size: ${stat.size}`);
+        let ext = path.extname(p);
+        if (ext === '.cbr' || ext === '.cbz') {
+          ret.push(p);
+        }
+      }
+    }
+    back.push(...ret.sort());
+  } // for
+  return back;
+}
+
+
+function save2disk(filename, data) {
+  let dir = path.dirname(filename);
+  shell.mkdir('-p', dir);
+
+  fs.writeFileSync(filename, data, (err) => {
     if (err) throw err;
     console.log('Saved!', file);
   });
 }
 
-export async function scan(dir) {
-
-  if (!dir) {
-    return log.error('nsd', 'Parameter dir is missing');
-  }
-
-  let p = path.normalize(dir);
-  log.info('nsd', 'Scanning ', p);
-
-  const walkOptions = {
-    return_object: true,
-    no_return: false,
-    find_links: false,
-    filter: (p, files) => {
-      // console.info('path', p); // TODO weg
-      // console.info('       files', files); // TODO weg
-
-      if (p.indexOf('.') === 0) return [];
-
-      return files;
-      // return files.filter(n => {
-      //   let ext = path.extname(n);
-      //   return ext === '.js'; // || n.indexOf('.') === -1;
-      // });
-    }
-  };
-  let result = await walk.async(dir, walkOptions);
-
-  //console.info();
-  // console.info('\n\n--------------------');
-  // let count = 0;
-  // let js = 0;
-  const ret = [];
-  for (let [p, stat] of Object.entries(result)) {
-    // if (path.endsWith('.js')) {
-    if (stat.size > 0) {
-      // log.info(`${path} mode:${stat.mode} size: ${stat.size}`);
-      let ext = path.extname(p);
-      if (ext === '.js') {
-        ret.push(p);
-      }
-      // js++;
-    }
-    // count++;
-  }
-  // log.info('count', count);
-  // log.info('js', js);
-  return ret.sort();
-}
-
-
 async function test() {
-  let ret = await scan('./');
-  console.info(ret);
-  writeFile('./out/demo.txt', ret);
+  let rows = await scan([
+    'e:/leeching/',
+    'y:/ebooks/comics/_deu/__temp'
+  ]);
+
+  save2disk('./out/files.txt', rows.join('\n'));
+
+  const o = createStructure(rows);
+  save2disk('./out/struc.json', JSON.stringify(o, null, 2));
+
+  let actions = regroup(o);
+  save2disk('./out/actions.json', JSON.stringify(actions, null, 2));
 }
 
 test();
---
+++ regroup.mjs
@@ -0,0 +1,15 @@
+"use strict";
+
+import path from "path";
+import fs from "fs";
+import log from "npmlog";
+
+
+export function regroup(object) {
+
+  const ret = {};
+
+
+
+  return ret;
+}
